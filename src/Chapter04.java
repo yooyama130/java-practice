750fa0ce5e1259d09c480c826c6b29d03ee18b9b
unknown <92510176+yooyama130@users.noreply.github.com>
2022-02-09 15:11:24 +0900

    [Finish] Chapter04

---
+++ src/Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "‘åŽR—z•½";
+		System.out.println(str);
+		str = "Žv‚¢‚â‚è";
+		System.out.println(str);
+		final int luckyNum = 777;
+		System.out.println(luckyNum);
+	}
+}
