2c0a81ac6f35159b8991da2f5fb0c50e3590d67d
unknown <92510176+yooyama130@users.noreply.github.com>
2022-02-10 13:16:29 +0900

    [Finish] Chapter05

---
+++ src/Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 530000;
+		double d = 3.14;
+		boolean b = true;
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+		
+		Float F = 1.31f;
+		Integer I = 10;
+		Character C = 'a';
+		System.out.println(F);
+		System.out.println(I);
+		System.out.println(C);
+		
+		int array[] = {0,1,2,3,4}; 
+		System.out.println(array[3]);
+	}
+}
