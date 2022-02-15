3c4d32580b9a5c00b41209659c3cdb9d55415264
unknown <92510176+yooyama130@users.noreply.github.com>
2022-02-15 14:01:12 +0900

    [Finish] Chapter12

---
+++ src/Chapter12.java
@@ -0,0 +1,9 @@
+public class Chapter12 {
+    public static void main(String[] args) {
+        Chapter12_CapsuleMan man = new Chapter12_CapsuleMan("小林", 32);
+        System.out.println("名前は" + man.getName() +  "です。");
+        System.out.println("年齢は" + man.getAge() + "です。");
+        man.setAge(33);
+        System.out.println("誕生日を迎えたので、年齢が" + man.getAge() + "になりました。");
+    }
+}
---
+++ src/Chapter12_CapsuleMan.java
@@ -0,0 +1,23 @@
+
+public class Chapter12_CapsuleMan {
+    private String name;
+    private int age;
+
+    public Chapter12_CapsuleMan(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+    
+}
